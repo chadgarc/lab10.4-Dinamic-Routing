ed96ccde3371c13618c43dc4416c50b482c90414
Christian Garlen <chadgarval@outlook.com>
2026-09-14 17:34:24 -0400

    refactor: restructure page components, add DataContext provider, and introduce BlogPost element

--- src/types/index.ts
+++ src/types/index.ts
@@ -11,7 +11,14 @@ export interface Post{
     slug: string,
     content: string,
     userId?: number,
+    user?: User,
     reactions?: Reactions
+}
+
+export interface User {
+    id: number,
+    username: string,
+    color: string
 }
 
 export interface AuthContextType {
